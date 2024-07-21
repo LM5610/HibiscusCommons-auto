8c5a77c591de702b7de72215a6e6e50cd12b7e51
LM5610 <88904242+LM5610@users.noreply.github.com>
2024-07-21 14:24:20 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,8 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 plugins {
     id("java")
     id("maven-publish")
-    id("io.github.goooler.shadow") version "8.1.7" // Temp shadow repo with fix
-    //id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.github.goooler.shadow") version "8.1.7"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
-    //id("io.papermc.hangar-publish-plugin") version "0.1.1"
     id("xyz.jpenilla.run-paper") version "2.0.0"
     id("io.papermc.paperweight.userdev") version "1.7.1" apply false
 }
@@ -23,36 +21,16 @@ allprojects {
         maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
         maven("https://oss.sonatype.org/content/groups/public/")
         maven("https://oss.sonatype.org/content/repositories/snapshots")
-
-        // UpdateChecker
-        maven("https://hub.jeff-media.com/nexus/repository/jeff-media-public/")
-
-        // Geary & Backup ProtocolLib repo
+        maven("https://repo.jeff-media.com/public/")
         maven("https://repo.mineinabyss.com/releases/")
         maven("https://repo.mineinabyss.com/snapshots/")
-
-        // Citizens & Denizen
         maven("https://maven.citizensnpcs.co/repo")
-
-        // Jitpack
         maven("https://jitpack.io")
-
-        // md-5 Repo
         maven("https://repo.md-5.net/content/groups/public/")
-
-        // MMOItems
         maven("https://nexus.phoenixdevt.fr/repository/maven-public/")
-
-        // Eco-Suite/Auxilor Repo
         maven("https://repo.auxilor.io/repository/maven-public/")
-
-        // PlaceholderAPI
         maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-
-        // ProtocolLib
         maven("https://repo.dmulloy2.net/repository/public/")
-
-        // MythicMobs
         maven {
             url = uri("https://mvn.lumine.io/repository/maven-public")
             metadataSources {
@@ -63,8 +41,6 @@ allprojects {
 
     dependencies {
         compileOnly(fileTree("${project.rootDir}/lib") { include("*.jar") })
-
-        // Included externally
         compileOnly("com.mojang:authlib:1.5.25")
         compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
         compileOnly("org.jetbrains:annotations:24.1.0")
@@ -84,18 +60,15 @@ allprojects {
         compileOnly("com.github.Xiao-MoMi:Custom-Fishing:2.1.2")
         compileOnly("com.ticxo.modelengine:ModelEngine:R4.0.2")
 
-        // Lombok <3
         annotationProcessor("org.projectlombok:lombok:1.18.34")
         testCompileOnly("org.projectlombok:lombok:1.18.34")
         testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
 
-        // Spigot Auto Loader Libraries
         compileOnly("net.kyori:adventure-api:4.17.0")
         compileOnly("net.kyori:adventure-text-minimessage:4.17.0")
         compileOnly("net.kyori:adventure-platform-bukkit:4.3.3")
         compileOnly("org.apache.commons:commons-lang3:3.14.0")
 
-        // Shaded Dependencies
         implementation("org.spongepowered:configurate-yaml:4.2.0-SNAPSHOT")
         implementation("org.bstats:bstats-bukkit:3.0.2")
         implementation("com.jeff_media:SpigotUpdateChecker:3.0.0")
@@ -116,7 +89,6 @@ dependencies {
 tasks {
     compileJava {
         options.encoding = Charsets.UTF_8.name()
-        //options.release.set(17)
     }
 
     runServer {
@@ -163,8 +135,6 @@ tasks {
     }
 }
 
-// Handles generating the plugin yml
-
 bukkit {
     load = BukkitPluginDescription.PluginLoadOrder.POSTWORLD
     main = "me.lojosho.hibiscuscommons.HibiscusCommonsPlugin"
@@ -189,7 +159,7 @@ bukkit {
     )
     version = "${project.version}"
     loadBefore = listOf(
-        "Cosmin" // Fixes an issue with Cosmin loading before and taking /cosmetic, when messing with what we do.
+        "Cosmin"
     )
 
     libraries = listOf(
@@ -197,35 +167,8 @@ bukkit {
         "net.kyori:adventure-text-minimessage:4.17.0",
         "net.kyori:adventure-platform-bukkit:4.3.3",
         "org.apache.commons:commons-lang3:3.14.0"
-        //"org.spongepowered:configurate-yaml:4.2.0-SNAPSHOT" // Readd when 4.2.0 releases
     )
 }
-/*
-hangarPublish {
-    publications.register("plugin") {
-        version.set(project.version as String)
-        channel.set("Release")
-        if (project.version.toString().contains("-")) channel.set("Snapshot") // If its a dev build, it will have -dev on it
-        id.set("HibiscusCommons")
-        apiKey.set(System.getenv("HANGAR_API_TOKEN"))
-        platforms {
-            register(Platforms.PAPER) {
-                jar.set(tasks.jar.flatMap { it.archiveFile })
-
-                val versions: List<String> = listOf("1.18.2-1.20.4")
-                platformVersions.set(versions)
-
-                dependencies {
-                    hangar("ProtocolLib") {
-                        required.set(true)
-                    }
-                }
-            }
-        }
-    }
-}
-*/
-// Publishing stuff below here to a remote maven repo
 
 publishing {
     val publishData = PublishData(project)
